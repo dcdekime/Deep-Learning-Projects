c9130abcbc21987d97831e76496d950446bff542
David DeKime <ddekime3@gatech.edu>
2021-04-25 02:10:45 -0400

    removing extra files

--- Dog Classifier/DogBreedClassifier.py
+++
@@ -1,9 +0,0 @@
-import os
-from torchvision import datasets
-import torchvision.models as models
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
-## TODO: Specify model architecture
-
